49b750d0f82d7322612c0177c2d02e1456f6a7cc
Prakash Raman <prakash.raman.ka@gmail.com>
2016-02-20 15:09:58 +0530

    made all public

--- Pod/Classes/PineSelectGroupItem.swift
+++ Pod/Classes/PineSelectGroupItem.swift
@@ -10,91 +10,91 @@ import UIKit
 import SnapKit
 
 public class PineSelectGroupItem : UIView {
-        
+
     public var data : Dictionary<String, String> = Dictionary<String, String>()
-    
+
     public enum State {
         case Active, Inactive
     }
-    
-    let label = UILabel()
-    let icon = PineIcon(named: "checkbox_inactive")
-    var text: String = ""
-    var value: String = ""
-    var state: State = .Inactive
-    var group: PineSelectGroup? = nil
-    
-    
+
+    public let label = UILabel()
+    public let icon = PineIcon(named: "checkbox_inactive")
+    public var text: String = ""
+    public var value: String = ""
+    public var state: State = .Inactive
+    public var group: PineSelectGroup? = nil
+
+
     public init(data: Dictionary<String, String>, group: PineSelectGroup){
         super.init(frame: CGRect.zero)
         self.data = data
         self.text = data["text"] ?? ""
         self.value = data["value"] ?? ""
         self.group = group
-        
+
         setup()
     }
-    
-    
+
+
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public func setup(){
         self.addSubview(self.label)
         self.addSubview(self.icon)
-        
+
         drawSelf()
         observeTouch()
         update()
     }
-    
+
     public func drawSelf(){
         drawLabel()
         drawIcon()
     }
-    
+
     public func drawLabel(){
         self.label.snp_makeConstraints { (make) -> Void in
             make.centerY.left.equalTo(self)
         }
     }
-    
+
     public func drawIcon(){
         self.icon.snp_makeConstraints { (make) -> Void in
             make.right.centerY.equalTo(self)
             make.size.equalTo(PineConfig.Size.icon)
         }
     }
-    
+
     func observeTouch(){
         let gesture = UITapGestureRecognizer(target: self, action: "tapped:")
         self.addGestureRecognizer(gesture)
     }
-    
+
     func tapped(sender: UITapGestureRecognizer? = nil){
         let s : State = (self.state == .Active) ? .Inactive : .Active
         set(state: s).beforeUpdate().update()
         self.group?.onSelection()
     }
-    
+
     public func set(state state: State) -> PineSelectGroupItem {
         self.state = state
         return self
     }
-    
+
     public func isActive() -> Bool {
         return self.state == .Active ? true : false
     }
-    
+
     public func beforeUpdate() -> PineSelectGroupItem {
         return self
     }
-    
+
     public func update(){
         self.label.text = self.text
         let iconname = self.isActive() ? "checkbox_active" : "checkbox_inactive"
         self.icon.change(named: iconname)
     }
-    
+
 }
