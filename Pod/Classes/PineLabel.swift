451df855cb11f03bb80dfc3d7f037de060f7ff38
Prakash Raman <prakash.raman.ka@gmail.com>
2016-02-25 00:00:04 +0530

    public adde to style

--- Pod/Classes/PineLabel.swift
+++ Pod/Classes/PineLabel.swift
@@ -11,29 +11,29 @@ import UIKit
 
 
 public class PineLabel : UILabel {
-    
+
     public init(){
         super.init(frame: CGRect.zero)
         setup()
         style()
     }
-    
+
     convenience public init(text: String){
         self.init()
         self.text = text
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func setup(){
         self.font = PineConfig.Font.get(PineConfig.Font.BOLD, size: 14)
     }
-    
+
     // THIS FUNCTION SHOULD BE OVERRIDDEN IN CASE OF SUB CLASSES
-    func style(){
+    public func style(){
 
     }
-    
+
 }
