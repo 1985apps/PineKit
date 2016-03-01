37f66b11fb14ed82297619bbc6cc55746cfd0506
Prakash Raman <prakash.raman.ka@gmail.com>
2016-03-01 15:54:13 +0530

    fixing load content view controller

--- Pod/Classes/PineMenuViewController.swift
+++ Pod/Classes/PineMenuViewController.swift
@@ -11,21 +11,21 @@ import UIKit
 
 
 public class PineMenuViewController : UIViewController, PineMenuDelegate  {
-    
+
     public var menuView: PineBaseMenuView?
     public var contentNavigationController: UINavigationController?
     public var transition = PineMenuTransition()
     public var rootViewController = PineBaseViewController()
-    
+
     public init(menuView: PineBaseMenuView, rootViewController: PineBaseViewController, transition: PineMenuTransition = PineMenuTransition()){
-        
+
         super.init(nibName: nil, bundle: nil)
-        
+
         self.menuView = menuView
         self.rootViewController = rootViewController
         self.transition = transition
         self.transition.setController(self)
-        
+
         // SET THE DELEGATE TO THE MENU VIEW
         self.menuView!.delegate = self
     }
@@ -33,59 +33,58 @@ public class PineMenuViewController : UIViewController, PineMenuDelegate  {
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
      public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setup()
     }
-    
+
     override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     func setup(){
         self.view.addSubview(self.menuView!)
         setupNavigationController()
     }
-    
+
     func setupNavigationController(){
         self.contentNavigationController = UINavigationController(rootViewController: self.rootViewController)
 
         self.addChildViewController(self.contentNavigationController!)
         self.view.addSubview(self.contentNavigationController!.view)
         self.contentNavigationController!.didMoveToParentViewController(self)
-        
+
         // SET THE FRAME TO THE PARENT FRAME
         // THIS DOES NOT USE AUTOLAYOUT - AS THERE WILL BE ANIMATIONS ON THE FRAME
         self.contentNavigationController!.view.frame = self.view.frame
-        
-        
+
+
         // SETUP THE BACKGROUND COLOR OF NAVBAR
         self.contentNavigationController!.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
         self.contentNavigationController!.navigationBar.shadowImage = UIImage()
         self.contentNavigationController!.navigationBar.translucent = true
         self.contentNavigationController?.navigationBar.tintColor = UIColor.redColor()
     }
-    
+
     func toggleMenu(){
         self.transition.toggle()
     }
-    
-    
+
+
     /* DELEGATE METHODS public */
     public func loadContentViewController(viewController: PineBaseViewController) {
-        let last = self.contentNavigationController?.viewControllers.last as! PineBaseViewController
-        last.pushViewController(viewController)
+        self.contentNavigationController?.setViewControllers([viewController], animated: true)
         self.transition.close()
     }
-    
+
     override public func preferredStatusBarStyle() -> UIStatusBarStyle {
         if let style = self.contentNavigationController?.viewControllers.last?.preferredStatusBarStyle() {
             return style
         }
         return .Default
     }
-    
+
 }
